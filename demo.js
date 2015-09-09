de79b77674b77cdf506f356ffb7cf445757e0c32
mattdesl <dave.des@gmail.com>
2015-09-09 11:21:03 -0400

    use standard style

--- demo.js
+++ demo.js
@@ -2,63 +2,58 @@ var touches = require('./')
 var css = require('dom-css')
 var xtend = require('xtend')
 
-require('domready')(function() {
-    var body = document.body
+require('domready')(function () {
+  var body = document.body
 
-    //create a simple box element
-    var div = create()
-    div.textContent = 'drag me!'
-    body.appendChild(div)
+  // create a simple box element
+  var div = create()
+  div.textContent = 'drag me!'
+  body.appendChild(div)
 
-    var child = create({ left: 250, background: 'red' })
-    body.appendChild(child)
+  var child = create({ left: 250, background: 'red' })
+  body.appendChild(child)
 
-    //start listening for drag events on window
-    //but use the div as our target element for position
-    var dragging = false
-    touches(window, { target: div, filtered: true })
-        .on('start', function(ev, pos) {
-            ev.preventDefault()
-            dragging = within(pos, div)
-            if (dragging)
-                write("start", pos)
-        })
-        .on('end', function(ev, pos) {
-            if (dragging)
-                write('end', pos)
-            dragging = false
-        })
-        .on('move', function(ev, pos) {
-            if (dragging)
-                write('move', pos)
-        })
+  // start listening for drag events on window
+  // but use the div as our target element for position
+  var dragging = false
+  touches(window, { target: div, filtered: true })
+    .on('start', function (ev, pos) {
+      ev.preventDefault()
+      dragging = within(pos, div)
+      if (dragging) write('start', pos)
+    })
+    .on('end', function (ev, pos) {
+      if (dragging) write('end', pos)
+      dragging = false
+    })
+    .on('move', function (ev, pos) {
+      if (dragging) write('move', pos)
+    })
 
-    function write(msg, pos) {
-        div.textContent = [msg, pos.map(Math.round).join(', ')].join(' ')
-    }
+  function write (msg, pos) {
+    div.textContent = [msg, pos.map(Math.round).join(', ')].join(' ')
+  }
 })
 
-function within(pos, element) {
-    var rect = element.getBoundingClientRect()
-    return pos[0] >= 0
-        && pos[1] >= 0
-        && pos[0] < rect.width
-        && pos[1] < rect.height
+function within (pos, element) {
+  var rect = element.getBoundingClientRect()
+  return pos[0] >= 0 && pos[1] >= 0 &&
+    pos[0] < rect.width && pos[1] < rect.height
 }
 
-function create(opt) {
-    var div = document.createElement('div')
-    css(div, xtend({
-        position: 'absolute',
-        top: 20,
-        left: 20,
-        background: 'gray',
-        width: 200,
-        height: 200,
-        color: 'white',
-        fontSize: '30px',
-        lineHeight: '200px',
-        textAlign: 'center'
-    }, opt))
-    return div
+function create (opt) {
+  var div = document.createElement('div')
+  css(div, xtend({
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    background: 'gray',
+    width: 200,
+    height: 200,
+    color: 'white',
+    fontSize: '30px',
+    lineHeight: '200px',
+    textAlign: 'center'
+  }, opt))
+  return div
 }
--- index.js
+++ index.js
@@ -1,136 +1,139 @@
 var Emitter = require('events/')
 
 var allEvents = [
-    'touchstart', 'touchmove', 'touchend', 'touchcancel',
-    'mousedown', 'mousemove', 'mouseup'
+  'touchstart', 'touchmove', 'touchend', 'touchcancel',
+  'mousedown', 'mousemove', 'mouseup'
 ]
 
 var ROOT = { left: 0, top: 0 }
 
-module.exports = function handler(element, opt) {
-    opt = opt||{}
-    element = element || window
+module.exports = function handler (element, opt) {
+  opt = opt || {}
+  element = element || window
 
-    var emitter = new Emitter()
-    emitter.target = opt.target || element
+  var emitter = new Emitter()
+  emitter.target = opt.target || element
 
-    var touch = null, which = null
-    var filtered = opt.filtered
+  var touch = null
+  var filtered = opt.filtered
 
-    var events = allEvents
+  var events = allEvents
 
-    //only a subset of events
-    if (typeof opt.type === 'string') {
-        events = allEvents.filter(function(type) {
-            return type.indexOf(opt.type) === 0
-        })
-    }
-
-    //grab the event functions
-    var funcs = events.map(function(type) {
-        var name = normalize(type)
-        var fn = function(ev) {
-            var client = ev
-            if (/^touch/.test(type)) {
-                if (filtered)
-                    client = getFilteredTouch(ev, type)
-                else
-                    client = getTargetTouch(ev.changedTouches, emitter.target)
-            }
-
-            if (!client)
-                return
-
-            //get 2D position
-            var pos = offset(client, emitter.target)
-
-            //dispatch the normalized event to our emitter
-            emitter.emit(name, ev, pos)
-        }
-        return { type: type, listener: fn }
+  // only a subset of events
+  if (typeof opt.type === 'string') {
+    events = allEvents.filter(function (type) {
+      return type.indexOf(opt.type) === 0
     })
+  }
 
-    emitter.enable = function enable() {
-        funcs.forEach(listeners(element, true))
+  // grab the event functions
+  var funcs = events.map(function (type) {
+    var name = normalize(type)
+    var fn = function (ev) {
+      var client = ev
+      if (/^touch/.test(type)) {
+        if (filtered) {
+          client = getFilteredTouch(ev, type)
+        } else {
+          client = getTargetTouch(ev.changedTouches, emitter.target)
+        }
+      }
 
-        return emitter
+      if (!client) {
+        return
+      }
+
+      // get 2D position
+      var pos = offset(client, emitter.target)
+
+      // dispatch the normalized event to our emitter
+      emitter.emit(name, ev, pos)
     }
+    return { type: type, listener: fn }
+  })
 
-    emitter.disable = function dispose() {
-        funcs.forEach(listeners(element, false))
+  emitter.enable = function enable () {
+    funcs.forEach(listeners(element, true))
 
-        return emitter
-    }
-
-    //initially enabled
-    emitter.enable()
     return emitter
+  }
 
-    function getFilteredTouch(ev, type) {
-        var client
+  emitter.disable = function dispose () {
+    funcs.forEach(listeners(element, false))
 
-        //clear touch if it was lifted or canceled
-        if (touch && /^touch(end|cancel)/.test(type)) {
-            //allow end event to trigger on tracked touch
-            client = getTouch(ev.changedTouches, touch.identifier||0)
-            if (client)
-                touch = null
-        }
-        //not yet tracking any touches, pick one from target
-        else if (!touch && /^touchstart/.test(type)) {
-            touch = client = getTargetTouch(ev.changedTouches, emitter.target)
-        }
-        //get the tracked touch
-        else if (touch)
-            client = getTouch(ev.changedTouches, touch.identifier||0)
+    return emitter
+  }
 
-        return client
+  // initially enabled
+  emitter.enable()
+  return emitter
+
+  function getFilteredTouch (ev, type) {
+    var client
+
+    // clear touch if it was lifted or canceled
+    if (touch && /^touch(end|cancel)/.test(type)) {
+      // allow end event to trigger on tracked touch
+      client = getTouch(ev.changedTouches, touch.identifier || 0)
+      if (client) {
+        touch = null
+      }
+    } else if (!touch && /^touchstart/.test(type)) {
+      // not yet tracking any touches, pick one from target
+      touch = client = getTargetTouch(ev.changedTouches, emitter.target)
+    } else if (touch) {
+    // get the tracked touch
+      client = getTouch(ev.changedTouches, touch.identifier || 0)
     }
+
+    return client
+  }
 }
 
-//get 2D client position of touch/mouse event
-function offset(ev, target) {
-    var cx = ev.clientX||0
-    var cy = ev.clientY||0
-    var rect = bounds(target)
-    return [ cx - rect.left, cy - rect.top ]
+// get 2D client position of touch/mouse event
+function offset (ev, target) {
+  var cx = ev.clientX || 0
+  var cy = ev.clientY || 0
+  var rect = bounds(target)
+  return [ cx - rect.left, cy - rect.top ]
 }
 
-//since we are adding events to a parent we can't rely on targetTouches
-function getTargetTouch(touches, target) {
-    return Array.prototype.slice.call(touches).filter(function(t) {
-        return t.target === target
-    })[0] || touches[0]
+// since we are adding events to a parent we can't rely on targetTouches
+function getTargetTouch (touches, target) {
+  return Array.prototype.slice.call(touches).filter(function (t) {
+    return t.target === target
+  })[0] || touches[0]
 }
 
-function getTouch(touches, id) {
-    for (var i=0; i<touches.length; i++)
-        if (touches[i].identifier === id)
-            return touches[i]
-    return null
-}
-
-function listeners(e, enabled) {
-    return function(data) {
-        if (enabled)
-            e.addEventListener(data.type, data.listener)
-        else
-            e.removeEventListener(data.type, data.listener)
+function getTouch (touches, id) {
+  for (var i = 0; i < touches.length; i++) {
+    if (touches[i].identifier === id) {
+      return touches[i]
     }
+  }
+  return null
 }
 
-//normalize touchstart/mousedown to "start" etc
-function normalize(event) {
-    return event.replace(/^(touch|mouse)/, '')
-     .replace(/up$/, 'end')
-     .replace(/down$/, 'start')
+function listeners (e, enabled) {
+  return function (data) {
+    if (enabled) e.addEventListener(data.type, data.listener)
+    else e.removeEventListener(data.type, data.listener)
+  }
 }
 
-function bounds(element) {
-    if (element===window
-            ||element===document
-            ||element===document.body)
-        return ROOT
-    else
-        return element.getBoundingClientRect()
+// normalize touchstart/mousedown to "start" etc
+function normalize (event) {
+  return event.replace(/^(touch|mouse)/, '')
+    .replace(/up$/, 'end')
+    .replace(/down$/, 'start')
+}
+
+function bounds (element) {
+  if (element === window ||
+      element === document ||
+      element === document.body) {
+    return ROOT
+  } else {
+    return element.getBoundingClientRect()
+  }
 }
